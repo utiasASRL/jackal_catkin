5c12b7c06745ac9f0128ccb652c2781089afdd3c
HuguesTHOMAS <huguesthomas218@gmail.com>
2022-03-22 18:03:00 -0400

    .

--- scripts/point_slam.sh
+++ scripts/point_slam.sh
@@ -5,20 +5,24 @@ source /opt/ros/melodic/setup.bash
 source ~/catkin_ws/devel/setup.bash
 
 nohup=false
-newmap=false
+filtering=false
+mapfile=""
 
-while getopts nm option
+# Example of map files:  map_Myhal_1.ply  /  map_Myhal_5.ply
+
+while getopts nfm: option
 do
 case "${option}"
 in
 n) nohup=true;;
-m) newmap=true;;
+f) filtering=true;;
+m) mapfile=${OPTARG};;
 esac
 done
 
-mycommand="roslaunch point_slam point_slam.launch filter:=false"
-if [ "$newmap" = false ] ; then
-    mycommand="$mycommand init_map_path:=/home/administrator/1-Deep-Collider/results/runs/Myhal/map_Myhal_1.ply"
+mycommand="roslaunch point_slam point_slam.launch filter:=$filtering"
+if [ -n "$mapfile" ] ; then
+    mycommand="$mycommand init_map_path:=/home/administrator/1-Deep-Collider/results/runs/Myhal/$mapfile"
 fi
 
 echo "---------   $mycommand"
